8094f35034df8f8f41509bbe422fd9aad7dbcd7d
jramirez <ramirez.jocelyn73@yahoo.com>
2021-08-03 22:07:43 -0700

    adding juice serializer

--- backend-juice/app/controllers/juices_controller.rb
+++ backend-juice/app/controllers/juices_controller.rb
@@ -2,6 +2,7 @@ class JuicesController < ApplicationController
 
     def index
         juices = Juice.all 
+        render json: JuiceSerializer.new(juices)
     end
 
     def show
--- backend-juice/app/serializers/juice_serializer.rb
+++ backend-juice/app/serializers/juice_serializer.rb
@@ -1,5 +1,5 @@
 class JuiceSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name
-  
+  has_many :lists 
 end
