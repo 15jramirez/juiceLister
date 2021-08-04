e31de2463fa74fe4fee80763cb19c390917bea01
jramirez <ramirez.jocelyn73@yahoo.com>
2021-08-03 21:53:39 -0700

    creating index and show for juice

--- backend-juice/app/controllers/juices_controller.rb
+++ backend-juice/app/controllers/juices_controller.rb
@@ -1,2 +1,9 @@
 class JuicesController < ApplicationController
+
+    def index
+        juices = Juice.all 
+    end
+
+    def show
+    end
 end
--- backend-juice/app/models/juice.rb
+++ backend-juice/app/models/juice.rb
@@ -1,3 +1,5 @@
 class Juice < ApplicationRecord
     serialize :ingredients, Hash 
+    has_many :lists
+    has_many :users, through: :lists
 end
