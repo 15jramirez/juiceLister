bea4d0c627f08d0945fe7b7863f1e8d1cf6dbe16
jramirez <ramirez.jocelyn73@yahoo.com>
2021-08-12 16:13:24 -0700

    fixed text showing up when hovered, changed click event to mouse event for image changes

--- frontend/index.js
+++ frontend/index.js
@@ -1,18 +1,7 @@
 console.log('hello form src')
-const frontside = document.getElementsByClassName(`front-end`)[0]
-let backside = document.getElementsByClassName(`back-end`)[0]
 const juiceApi = new JuiceApi
-document.addEventListener(`DOMContentLoaded`, function(){
-    juiceApi.fetchImages()
-})
+juiceApi.fetchImages();
+// document.addEventListener(`DOMContentLoaded`, function(){
+//     juiceApi.fetchImages()
+// })
  
-// function addImages(result){
-//    // want to grab the each element of array of result. 
-//    result.forEach(juice => picture.innerHTML += `<img src = "${juice.image_url}" width= "400" height="200">`)
-//    picture.addEventListener(`click`, handleClickImage)
-
-// }
-
-// function handleClickImage(event){
-//     console.log(event)
-// }
--- frontend/src/juice.js
+++ frontend/src/juice.js
@@ -1,51 +1,51 @@
-// contains juice class. simlar to model handle all logica, info specific to juice
 class Juice{
     static all =[]
-    constructor({name, image_url, ingredients, id, category_id}){
-        this.name = name 
-        this.image_url = image_url
-        this.ingredients = ingredients
-        this.id = id 
-        this.category_id = category_id
-        this.element = document.createElement(`div`)
+    constructor({name, image_url, ingredients, id, category_id}){    //deconstructed the {} allows to put elements w/ attributes
+        this.name = name    // this will also display on the back with image card
+        this.image_url = image_url  // front of card
+        this.ingredients = ingredients  //back of card 
+        this.id = id //id of juice instance
+        this.category_id = category_id  // want to put this on back of card too
+        this.element = document.createElement(`div`)  // creating image with own div. this is front of card/(image is front)
         this.element.id = `juice-${this.id}`
-        this.element.classList.add(`imageFront`)
-        Juice.all.push(this)
+        // this.element.classList.add(`imageFront`)  // adding class of imageFront to have FRONT CARD
+        Juice.all.push(this);
     }
-    get frontPicture(){
-        return  document.getElementById(`container`)
+    get imageContainer(){
+        return document.getElementById(`container-box`) //this to grab div to append 
     }
 
-    get imageCards(){
-        return Array.from(document.getElementsByClassName(`imageFront`))
+   
+    static findByID(){
+        return Juice.all.find(juice =>juice.id == id)
     }
     addImagesToDom(){
-        this.frontPicture.append(this.fullRender())  //this is for when make changes, it'll only rerender and not add divs
         this.addEventListeners()
+        this.imageContainer.append(this.fullRender())  //this is for when make changes, it'll only rerender and not add divs
     }
 
     fullRender(){
-        this.element.innerHTML = `<img src = "${this.image_url}" width= "400" height="200">`
-        return this.element
+        console.log(this)
+        this.element.innerHTML = `<img src = "${this.image_url}" width= "400" height="200"> 
+        <p><span>Juice: ${this.name}</span> is great ${this.category_id}. It has all of this ingridents</p>`
+        return this.element // need to return otherwise returns undefined
     }
 
     addEventListeners(){
-        console.log(this)
-        console.log(this.imageCards)
-        this.imageCards.addEventListener(`mouseenter`, function(){
-            this.classList.remove(`imageFront`)
-            this.classList.add(`imageBack`)
-        })
+        this.element.addEventListener(`mouseenter`, this.handleMouseEnterImage)
+        this.element.addEventListener(`mouseleave`, this.handleMouseLeaveImage)
+    }
+    handleMouseEnterImage(event){
+        console.log(event)
+        this.classList.remove('imageFront')
+        this.classList.add(`imageBack`)
 
-        this.frontPicture.addEventListener(`mouseleave`, function(){
-            this.classList.add(`imageFront`) 
-            this.classList.remove(`imageBack`)
-        })
-        // this.element.addEventListener(`click`, this.handlePictureClick)
     }
 
-    handlePictureClick(){
-        
+    handleMouseLeaveImage(event){
+        console.log(event)
+        this.classList.add('imageFront')
+        this.classList.remove(`imageBack`)
     }
 
 }
