9716d8dcbb087674ef455d2671132345a93b3394
jramirez <ramirez.jocelyn73@yahoo.com>
2021-08-18 16:37:18 -0700

    added alret to show updated changes

--- backend/app/controllers/juices_controller.rb
+++ backend/app/controllers/juices_controller.rb
@@ -1,42 +1,45 @@
+require 'pry'
 class JuicesController < ApplicationController
     
-    # before_action :find_profile, only:[:show,:update,:destroy]
-
     def index
         render json: JuiceSerializer.new(Juice.all)
     end
 
     def create
-        juice = Juice.new(juice_params)
+        # whitelisted own params due to permit not allowed 
+        juice = Juice.create(
+            :name => params[:name],
+            :description => params[:description],
+            :category_id => params[:category],
+            :ingredients => params[:ingredients],
+            :image => params[:image_url]
+        )
         if juice.save 
-            render json: juice 
+            render json: JuiceSerializer.new(juice) 
         else
             render json: juice.errors, message: "Oops. Can't find it. Try your spelling"
         end
     end
 
     def show 
-        
-        render json: juice
+        juice = Juice.find(params[:id])
+       render json: JuiceSerializer.new(juice)
     end
 
-    # def update 
-    #    juice.image.attach(params[:image]) 
-    #     if valid?
-    #         juice.update(juice_params)
-    #     end
-    # end
+    def update
+    juice = Juice.find(params[:id])
+    juice.update(
+          :name => params[:name],
+         :description => params[:description],
+         :ingredients => params[:ingredients]
+         )
+    juice.save
+  render json: JuiceSerializer.new(juice)
+    end
 
     # def destroy 
     #    juice.destroy
     # end
 
-    private
-        def juice_params
-            params.require(:juice).permit(:name, :ingredients, :image)  #want to pass image
-        end
 
-        # def find_juice
-        #     juice = Juice.find(params[:id])
-        # end
 end
--- backend/app/serializers/juice_serializer.rb
+++ backend/app/serializers/juice_serializer.rb
@@ -1,4 +1,4 @@
 class JuiceSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :ingredients, :category_id, :image_url, :id
+  attributes :name, :ingredients, :category_id, :image_url, :id, :description  #need image_url tag to pass url
 end
